ac0b9b69a96f536dc06f256002fb26cb6b9b7e57
Ajeet2270028 <ajeetjaiswal75620@gmail.com>
2024-12-11 21:10:16 +0530

    Resoponse file created

--- src/main/java/com/example/vra/entity/User.java
+++ src/main/java/com/example/vra/entity/User.java
@@ -16,6 +16,48 @@ public class User {
  private String password;
  private UserRole role;
  private String profilePicture;
+public int getUserid() {
+	return userid;
+}
+public void setUserid(int userid) {
+	this.userid = userid;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPhoneNumber() {
+	return phoneNumber;
+}
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public UserRole getRole() {
+	return role;
+}
+public void setRole(UserRole role) {
+	this.role = role;
+}
+public String getProfilePicture() {
+	return profilePicture;
+}
+public void setProfilePicture(String profilePicture) {
+	this.profilePicture = profilePicture;
+}
  
  
 }
